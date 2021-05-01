8a7b59af409c1c0d6874703a30ac6fd453b51f68
deniscodes <piroko25>
2021-04-30 20:11:00 -0400

    implement: added logout and tokens to auth.js

--- auth.js
+++ auth.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 
 const accessTokenSecret = 'youraccesstokensecret';
+const refreshTokenSecret = 'yourrefreshtokensecrethere';
+const refreshTokens = [];
 
 const users = [
     {
@@ -25,20 +27,53 @@ app.listen(3000, () => {
 });
 
 app.post('/login', (req, res) => {
-    // Read username and password from request body
+    // read username and password from request body
     const { username, password } = req.body;
 
-    // Filter user from the users array by username and password
+    // filter user from the users array by username and password
     const user = users.find(u => { return u.username === username && u.password === password });
 
     if (user) {
-        // Generate an access token
-        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
 
         res.json({
-            accessToken
+            accessToken,
+            refreshToken
         });
     } else {
         res.send('Username or password incorrect');
     }
+});
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
+});
+app.post('/logout', (req, res) => {
+    const { token } = req.body;
+    refreshTokens = refreshTokens.filter(token => t !== token);
+
+    res.send("Logout successful");
 });

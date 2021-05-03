60833457213a32f6343bb0bba539667f15a4096a
deniscodes <piroko25>
2021-05-03 18:12:23 -0400

    fix: used '' instead of ""

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ getJSON('http://localhost:8000/api/v1/cities',
     if (err !== null) {
         alert('Something went wrong: ' + err);
     } else {
-        const table = document.querySelector("table");
+        const table = document.querySelector('table');
         const data = Object.keys((records.data[0]));
         const dataRecords = records.data;
         generateTableHead(table, data);
@@ -20,7 +20,7 @@ function generateTableHead(table, data) {
     const thead = table.createTHead();
     const row = thead.insertRow();
     for (const key of data) {
-        const th = document.createElement("th");
+        const th = document.createElement('th');
         const text = document.createTextNode(key);
         th.appendChild(text);
         row.appendChild(th);

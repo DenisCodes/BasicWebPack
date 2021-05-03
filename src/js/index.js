2115d1b41dac7a41cf909499570031ef4ba1bdb0
deniscodes <piroko25>
2021-05-03 18:19:50 -0400

    fix: reagranged file and added spaces

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,6 @@ import TestJS from './TestJs';
 import getJSON from './getJSON';
 
 TestJS();
-getJSON('http://localhost:8000/api/v1/cities',
-    function(err, records) {
-    if (err !== null) {
-        alert('Something went wrong: ' + err);
-    } else {
-        const table = document.querySelector('table');
-        const data = Object.keys((records.data[0]));
-        const dataRecords = records.data;
-        generateTableHead(table, data);
-        generateTable(table, dataRecords);
-    }
-
-    console.log(data);
-});
 function generateTableHead(table, data) {
     const thead = table.createTHead();
     const row = thead.insertRow();
@@ -38,3 +24,18 @@ function generateTable(table, data) {
         }
     }
 }
+
+getJSON('http://localhost:8000/api/v1/cities',
+    function(err, records) {
+        if (err !== null) {
+            alert('Something went wrong: ' + err);
+        } else {
+            const table = document.querySelector('table');
+            const data = Object.keys((records.data[0]));
+            const dataRecords = records.data;
+            generateTableHead(table, data);
+            generateTable(table, dataRecords);
+        }
+
+        console.log(data);
+    });

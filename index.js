7f7f4f9480683e5d5abc26f0cf87280a86e767f7
deniscodes <piroko25>
2021-05-09 14:02:13 -0400

    implementation: set up getter

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ const port = process.env.PORT || "8000";
  * Routes Definitions
  */
 
+app.get("/", (req, res) => {
+    res.status(200).send("WHATABYTE: Food For Devs");
+});
+
 /**
  * Server Activation
  */

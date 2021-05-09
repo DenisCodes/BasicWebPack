d0ed300a8454850fe008df5db5ece60fff466d36
deniscodes <piroko25>
2021-05-09 14:57:46 -0400

    implementation: added passport config

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const port = process.env.PORT || "8000";
 
 /**
- * Session Configuration (New!)
+ * Session Configuration
  */
 
 const session = {
@@ -37,10 +37,28 @@ if (app.get("env") === "production") {
 }
 
 /**
- * Passport Configuration (New!)
+ * Passport Configuration
  */
 
-
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
 
 /**
  *  App Configuration
